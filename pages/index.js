efcd6f7ddd417dad38ab89327e46f126d7f7ec82
Daniel Vitor <110069086+danielvitordev@users.noreply.github.com>
2025-05-26 17:58:30 +0000

    modifica a frase

--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,5 @@
 function Home() {
-    return <h1>Agora que você acessou o link, mandegit push um print da sua tela de volta para mim.</h1>
+    return <h1>Agora que você acessou o link, mande um print da sua tela de volta para mim.</h1>
 }
 
 export default Home
